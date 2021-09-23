3aa868ac83dc63773b3afc79b5beee6e8e6c70a9
baraahalomari <balomari995@gmail.com>
2021-09-23 23:40:30 +0300

    done

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ require('dotenv').config();
 
 const app = express();
 
-const postRouter= require('./routes/posts.js');
+const postRouter = require('./routes/posts.js');
 
 
-app.use(express.json({limit:"30mb",extended:true}));
-app.use(express.urlencoded({limit:"30mb",extended:true}));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.get('/',(req,res)=>{
+
+app.use('/post', postRouter);
+
+app.get('/', (req, res) => {
   res.send('Hello World')
 });
-app.use('/post',postRouter);
-
-
 
 
 
@@ -27,5 +27,5 @@ mongoose.connect(process.env.CONNECTION_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then (()=> app.listen(process.env.PORT, () => console.log(`Server started on ${process.env.PORT}`)))
-.catch((err)=> console.log(err.message))
+  .then(() => app.listen(process.env.PORT, () => console.log(`Server started on ${process.env.PORT}`)))
+  .catch((err) => console.log(err.message))
